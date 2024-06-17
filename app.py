02598a55f1a80c0fa6a2d57846ed6d73ef1a94c9
ViktorPrystai <viktor.prystai.21@pnu.edu.ua>
2024-06-17 13:52:35 +0300

    document generation has been updated

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask, request, send_file, render_template
-import os
+import re
 from docx import Document
+from flask import Flask, send_file, request, render_template
+import os
 from datetime import datetime
 
 app = Flask(__name__)
 
+months = {
+    1: "січня", 2: "лютого", 3: "березня", 4: "квітня",
+    5: "травня", 6: "червня", 7: "липня", 8: "серпня",
+    9: "вересня", 10: "жовтня", 11: "листопада", 12: "грудня"
+}
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/generate', methods=['POST'])
 def generate():
@@ -18,33 +26,54 @@ def generate():
     days = request.form['days']
     start_date = request.form['start_date']
     end_date = request.form['end_date']
-    
-    template_path = 'static/InputFiles/file.docx'
+
+    template_path = 'static/InputFiles/file2.docx'
     document = Document(template_path)
-    
-    for paragraph in document.paragraphs:
-        if '№ _____' in paragraph.text:
-            paragraph.text = paragraph.text.replace('№ _____', f'№ {doc_number}')
-        if 'від «____» ______________ 20___ року' in paragraph.text:
-            formatted_issue_date = datetime.strptime(issue_date, '%Y-%m-%d').strftime('%d.%m.%Y')
-            paragraph.text = paragraph.text.replace('від «____» ______________ 20___ року', f'від «{formatted_issue_date}» року')
-        if '______ курсу' in paragraph.text:
-            paragraph.text = paragraph.text.replace('______ курсу', f'{course} курсу')
-        if '______________ (прізвище власне ім’я по батькові за наявності))' in paragraph.text:
-            paragraph.text = paragraph.text.replace('______________ (прізвище власне ім’я по батькові за наявності))', f'{name}')
-        if 'строком на ______ днів' in paragraph.text:
-            paragraph.text = paragraph.text.replace('строком на ______ днів', f'строком на {days} днів')
-        if 'з «____» _________________ 20___ року по «____» _________________ 20___ року' in paragraph.text:
-            formatted_start_date = datetime.strptime(start_date, '%Y-%m-%d').strftime('%d.%m.%Y')
-            formatted_end_date = datetime.strptime(end_date, '%Y-%m-%d').strftime('%d.%m.%Y')
-            paragraph.text = paragraph.text.replace('з «____» _________________ 20___ року по «____» _________________ 20___ року', f'з «{formatted_start_date}» року по «{formatted_end_date}» року')
-    
-    output_path = os.path.join('uploads', f'call_{doc_number}.docx')
+
+    process_elements(document, doc_number, issue_date, course, name, days, start_date, end_date)
+
+    output_path = os.path.join('uploads', f'Довідка_{name}_{doc_number}.docx')
     document.save(output_path)
-    
+
     return send_file(output_path, as_attachment=True)
+
+
+def process_elements(document, doc_number, issue_date, course, name, days, start_date, end_date):
+
+    for paragraph in document.paragraphs:
+        replace_placeholders(paragraph, doc_number, issue_date, course, name, days, start_date, end_date)
+
+
+def replace_placeholders(paragraph, doc_number, issue_date, course, name, days, start_date, end_date):
+    text = paragraph.text
+
+    date_obj = datetime.strptime(issue_date, "%Y-%m-%d")
+    start_date_obj = datetime.strptime(start_date, "%Y-%m-%d")
+    end_date_obj = datetime.strptime(end_date, "%Y-%m-%d")
+
+    formatted_date = f'від «{date_obj.day}» {date_obj.month} {date_obj.year} року'
+    formatted_start_date = f'з «{start_date_obj.day}» {months[start_date_obj.month]} {start_date_obj.year} року'
+    formatted_end_date = f'по «{end_date_obj.day}» {months[end_date_obj.month]} {end_date_obj.year} року'
+
+    text = re.sub(r'\{\{doc_number\}\}', doc_number, text)
+    text = re.sub(r'\{\{issue_date\}\}', formatted_date, text)
+    text = re.sub(r'\{\{course\}\}', course, text)
+    text = re.sub(r'\{\{name\}\}', name, text)
+    text = re.sub(r'\{\{days\}\}', str(days), text)
+    text = re.sub(r'\{\{start_date\}\}', formatted_start_date, text)
+    text = re.sub(r'\{\{end_date\}\}', formatted_end_date, text)
+    paragraph.text = text
+
 
 if __name__ == '__main__':
     if not os.path.exists('uploads'):
         os.makedirs('uploads')
     app.run(debug=True)
+
+
+
+
+
+
+
+
