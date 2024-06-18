b5812f205d10dee732958a65e61d5e8ce6ed3d52
Marian <pidchashymaryan@gmail.com>
2024-06-18 20:37:25 +0300

    calculating amound of days according to start and end dates

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
+from flask import Flask, send_file, request, render_template
 import re
 from docx import Document
-from flask import Flask, send_file, request, render_template
 import os
 from datetime import datetime
 
@@ -11,6 +11,7 @@ months = {
     5: "травня", 6: "червня", 7: "липня", 8: "серпня",
     9: "вересня", 10: "жовтня", 11: "листопада", 12: "грудня"
 }
+
 
 @app.route('/')
 def index():
@@ -23,14 +24,13 @@ def generate():
     issue_date = request.form['issue_date']
     course = request.form['course']
     name = request.form['name']
-    days = request.form['days']
     start_date = request.form['start_date']
     end_date = request.form['end_date']
 
     template_path = 'static/InputFiles/file2.docx'
     document = Document(template_path)
 
-    process_elements(document, doc_number, issue_date, course, name, days, start_date, end_date)
+    process_elements(document, doc_number, issue_date, course, name, start_date, end_date)
 
     output_path = os.path.join('uploads', f'Довідка_{name}_{doc_number}.docx')
     document.save(output_path)
@@ -38,18 +38,19 @@ def generate():
     return send_file(output_path, as_attachment=True)
 
 
-def process_elements(document, doc_number, issue_date, course, name, days, start_date, end_date):
-
+def process_elements(document, doc_number, issue_date, course, name, start_date, end_date):
     for paragraph in document.paragraphs:
-        replace_placeholders(paragraph, doc_number, issue_date, course, name, days, start_date, end_date)
+        replace_placeholders(paragraph, doc_number, issue_date, course, name, start_date, end_date)
 
 
-def replace_placeholders(paragraph, doc_number, issue_date, course, name, days, start_date, end_date):
+def replace_placeholders(paragraph, doc_number, issue_date, course, name, start_date, end_date):
     text = paragraph.text
 
     date_obj = datetime.strptime(issue_date, "%Y-%m-%d")
     start_date_obj = datetime.strptime(start_date, "%Y-%m-%d")
     end_date_obj = datetime.strptime(end_date, "%Y-%m-%d")
+
+    daysObj = end_date_obj - start_date_obj
 
     formatted_date = f'від «{date_obj.day}» {date_obj.month} {date_obj.year} року'
     formatted_start_date = f'з «{start_date_obj.day}» {months[start_date_obj.month]} {start_date_obj.year} року'
@@ -59,7 +60,7 @@ def replace_placeholders(paragraph, doc_number, issue_date, course, name, days, 
     text = re.sub(r'\{\{issue_date\}\}', formatted_date, text)
     text = re.sub(r'\{\{course\}\}', course, text)
     text = re.sub(r'\{\{name\}\}', name, text)
-    text = re.sub(r'\{\{days\}\}', str(days), text)
+    text = re.sub(r'\{\{days\}\}', str(daysObj.days), text)
     text = re.sub(r'\{\{start_date\}\}', formatted_start_date, text)
     text = re.sub(r'\{\{end_date\}\}', formatted_end_date, text)
     paragraph.text = text
@@ -69,11 +70,3 @@ if __name__ == '__main__':
     if not os.path.exists('uploads'):
         os.makedirs('uploads')
     app.run(debug=True)
-
-
-
-
-
-
-
-
